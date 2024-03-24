d3a735b5a72b71feb938b1c6cdf4779cdf6a36c2
midhunps <midhunps.0@gmail.com>
2024-03-24 18:43:42 +0530

    updated cycle component text size

--- resources/views/components/cycle-component.blade.php
+++ resources/views/components/cycle-component.blade.php
@@ -3,6 +3,6 @@
         <div class="flex justify-center ">
             <x-easyadmin::display.icon icon="{{$icon}}" height="h-10" width="w-8 sm:w-10" />
         </div>
-        <h4 class="font-medium hover:font-bold text-base xl:text-lg font-questrial text-center
+        <h4 class="font-medium hover:font-bold text-base font-questrial text-center
             ">{{$title}}</h4>
 </div>
