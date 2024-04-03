e87ee8a35aabc4b7d9e736ba89a4a39474314702
midhunps <midhunps.0@gmail.com>
2024-04-03 16:43:52 +0530

    Corrected null classes attribs issue in images render

--- resources/views/components/contentbuilder/image.blade.php
+++ resources/views/components/contentbuilder/image.blade.php
@@ -1,6 +1,6 @@
 @props(['data'])
 <div class="img cb-img w-full">
-    <img class="w-full {{$data->attribs->classes}}" src="{{$data->url}}"
+    <img class="w-full {{$data->attribs->classes ?? ''}}" src="{{$data->url}}"
         alt="{{$data->attribs->alt}}"
         width="{{$data->attribs->width}}"
         height="{{$data->attribs->height}}">
