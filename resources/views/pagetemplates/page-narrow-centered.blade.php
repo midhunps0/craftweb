4afeb9b355552fcde49811e9a28f25827c87dacb
midhunps <midhunps.0@gmail.com>
2024-04-09 17:13:29 +0530

    modified pagetemplates for full-width and narrow

--- resources/views/pagetemplates/page-narrow-centered.blade.php
+++ resources/views/pagetemplates/page-narrow-centered.blade.php
@@ -5,7 +5,7 @@
         <div>
             <img src="{{$instance->current_translation->display_image}}" alt="">
         </div>
-        <div>
+        <div class="max-w-3/4 m-auto">
             <x-contentbuilder.renderer :content="json_decode($instance->current_translation->data['body'])"/>
         </div>
     </div>
