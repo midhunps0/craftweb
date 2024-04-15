76e0b5ab39e0d425853e796724eeeb626ce9a105
midhunps <midhunps.0@gmail.com>
2024-04-15 14:25:58 +0530

    modified page-narrow

--- resources/views/pagetemplates/page-narrow-centered.blade.php
+++ resources/views/pagetemplates/page-narrow-centered.blade.php
@@ -3,7 +3,7 @@
         <x-main-menu-component />
         <div class="w-full px-2 md:px-16 lg:px-24">
             <x-page-title title="{{$instance->current_translation->data['title']}}" />
-            <div class="md:w-3/4 lg:w-2/3">
+            <div class="md:w-3/4 m-auto">
                 @if (isset($instance->current_translation->display_image))
                     <div>
                         <img src="{{$instance->current_translation->display_image}}" alt="">
