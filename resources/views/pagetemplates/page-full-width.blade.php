ae8333186ddbec6559c59ce5b2369ff06c797d38
midhunps <midhunps.0@gmail.com>
2024-04-15 14:21:18 +0530

    modified narrow and wide templates

--- resources/views/pagetemplates/page-full-width.blade.php
+++ resources/views/pagetemplates/page-full-width.blade.php
@@ -1,11 +1,13 @@
 <x-guest-layout>
-    <div>
+    <div class="text-base">
         <x-main-menu-component />
         <div class="w-full px-2 md:px-16 lg:px-24">
             <x-page-title title="{{$instance->current_translation->data['title']}}" />
-            <div>
-                <img src="{{$instance->current_translation->display_image}}" alt="">
-            </div>
+            @if (isset($instance->current_translation->display_image))
+                <div>
+                    <img src="{{$instance->current_translation->display_image}}" alt="">
+                </div>
+            @endif
             <div>
                 <x-contentbuilder.renderer :content="json_decode($instance->current_translation->data['body'])"/>
             </div>
--- resources/views/pagetemplates/page-narrow-centered.blade.php
+++ resources/views/pagetemplates/page-narrow-centered.blade.php
@@ -3,11 +3,15 @@
         <x-main-menu-component />
         <div class="w-full px-2 md:px-16 lg:px-24">
             <x-page-title title="{{$instance->current_translation->data['title']}}" />
-            <div>
-                <img src="{{$instance->current_translation->display_image}}" alt="">
-            </div>
-            <div class="lg:max-w-2/3 m-auto">
-                <x-contentbuilder.renderer :content="json_decode($instance->current_translation->data['body'])"/>
+            <div class="md:w-3/4 lg:w-2/3">
+                @if (isset($instance->current_translation->display_image))
+                    <div>
+                        <img src="{{$instance->current_translation->display_image}}" alt="">
+                    </div>
+                @endif
+                <div>
+                    <x-contentbuilder.renderer :content="json_decode($instance->current_translation->data['body'])"/>
+                </div>
             </div>
         </div>
         <x-footer/>
