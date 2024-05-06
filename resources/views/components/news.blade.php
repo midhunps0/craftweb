13e18a5204f9fda819f72e4d0eb6eff936440d97
midhunps <midhunps.0@gmail.com>
2024-05-06 16:09:08 +0530

    modified newd details page component

--- resources/views/components/news.blade.php
+++ resources/views/components/news.blade.php
@@ -1,6 +1,6 @@
 @props(['title' => 'An announcement from Craft', 'img' => ''])
 <div class="m-2 p-4 max-w-full shadow-[3px_3px_4px_2px_rgba(0,0,0,0.2)] h-full">
-    <div @click="$dispatch('newsclick', {imgsrc: '{{$img}}'})" class="h-72 overflow-auto flex justify-center items-center">
+    <div @click="$dispatch('newsclick', {imgsrc: '{{$img}}'})" class="h-72 overflow-auto flex justify-center items-center border-lightgray">
         <img src="{{$img}}" alt="" class="w-full">
     </div>
     <div class="text-center p-2 mt-4">
