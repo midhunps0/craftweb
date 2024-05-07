546cea3b1f2e45c8fcda66240a361e5ed1807b48
Reshma K R <reshmakanjoorparambil@gmail.com>
2024-05-07 15:01:01 +0530

    commit after add image

---
+++ resources/views/components/bookingappointment-kochi.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-white/60 w-1/2 absolute text-sm bottom-0 left-0 p-4 lg:p-8 text-black flex flex-row gap-2 lg:gap-6 justify-center">
+    <div class="lg:flex flex-row items-center lg:gap-2 hidden">
+        <p class="text-xs">Emergency Contact<br> 0480 2800 300</p>
+        <span ><x-easyadmin::display.icon icon="icons.phone-fill-icon" height="h-4" width="w-4" /></span>
+    </div>
+    <div class="flex flex-row items-center gap-2 fill-current text-black">
+        <p class="text-xs">Book An Appointment</p>
+        <span class="fill-current text-black"><x-easyadmin::display.icon icon="icons.booking-icon" height="h-6" width="w-6" /><span>
+    </div>
+</div>
