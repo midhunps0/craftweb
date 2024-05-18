91a118a781810e335449ab0ba008905c2129de97
Reshma K R <reshmakanjoorparambil@gmail.com>
2024-05-18 17:01:25 +0530

    commit after update

--- resources/views/components/bookappointment-component.blade.php
+++ resources/views/components/bookappointment-component.blade.php
@@ -14,7 +14,7 @@
         <img src="/images/icons/envelopebgremove.png" class="h-8 md:h-12 mt-6 ml-2 md:ml-6" alt="">
         </div>
         <div class="items-center ml-4 md:ml-10 xl:ml-12 2xl:ml-12 4/5">
-            <p class="font-franklin font-medium items-center text-left text-xs pt-6 md:text-sm lg:text-base "><a href="{{route('webpages.guest.show', ['locale' => app()->currentLocale(), 'slug' => 'contact-us'])}}" @click.prevent.stop="$dispatch('linkaction', {link: '{{route('webpages.guest.show', ['locale' => app()->currentLocale(), 'slug' => 'contact-us'])}}', route: 'contact'})">{{ __('footer.send_us_message') }}<a></p>
+            <p class="font-franklin font-medium items-center text-left text-xl pt-6 md:text-2xl lg:text-3xl "><a href="{{route('webpages.guest.show', ['locale' => app()->currentLocale(), 'slug' => 'contact-us'])}}" @click.prevent.stop="$dispatch('linkaction', {link: '{{route('webpages.guest.show', ['locale' => app()->currentLocale(), 'slug' => 'contact-us'])}}', route: 'contact'})">{{ __('footer.send_us_message') }}<a></p>
             <p class="font-franklin font-medium items-center text-left text-xs md:text-sm lg:text-base">{{ __('footer.emails_craft') }}</p>
         </div>
 
