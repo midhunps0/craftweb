c1c02a5d35018233447ce5a59a01876df8f69fa4
midhunps <midhunps.0@gmail.com>
2024-06-17 14:39:26 +0530

    doctors page card modified watermark removed, build taken

--- resources/views/components/doctorcard-component.blade.php
+++ resources/views/components/doctorcard-component.blade.php
@@ -6,14 +6,14 @@
             {{$name}}
         </p>
      </div>
-      <div class="relative flex flex-row justify-end">
-         <img src="{{$photo_url}}" class="h-52 dir-img" alt="doctor_image">
+      <div class="relative flex flex-row justify-end w-full">
+         <img src="{{$photo_url}}" class="w-full min-h-52 dir-img" alt="doctor_image">
       </div>
 
-      <div class="absolute flex ltr:flex-row rtl:flex-row-reverse items-center space-x-2 -rotate-90 origin-top-left bottom-10 ltr:left-2 rtl:-right-24">
+      {{-- <div class="absolute flex ltr:flex-row rtl:flex-row-reverse items-center space-x-2 -rotate-90 origin-top-left bottom-10 ltr:left-2 rtl:-right-24">
             <img src="/images/icons/Copy of icone-d-information-noir.png" class="opacity-40 h-8 w-8 rotate-90" alt="">
             <p class="text-gray text-xs font-normal">{{__('homecontent.craft_ivf_hospital')}}</p>
-      </div>
+      </div> --}}
 
       <div class="bg-gray-600 py-3 h-20 overflow-hidden">
          <p class="text-white font-questrial font-normal text-sm text-center">{{$designation}}</p>
